3eb2bfd8824e1083bd7de5da5e846a6ac48e85cb
Emanuele Aliberti <dev@mtka.eu>
2024-05-17 17:51:11 +0200

    add missing decorations for OpenAPI

--- src/logs/dto/log-file-info.dto.ts
+++ src/logs/dto/log-file-info.dto.ts
@@ -1,8 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class LogFileInfoDTO {
+
+    @ApiProperty()
+    public readonly name: string;
+
+    @ApiProperty()
+    public readonly size: number;
+
+    @ApiProperty()
+    public readonly created: Date;
+
+    @ApiProperty()
+    public readonly modified: Date;
+
     public constructor(
-        public readonly name: string,
-        public readonly size: number,
-        public readonly created: Date,
-        public readonly modified: Date,
-    ) { }
+        name: string,
+        size: number,
+        created: Date,
+        modified: Date,
+    ) {
+        this.name = name;
+        this.size = size;
+        this.created = created;
+        this.modified = modified;
+    }
 }
