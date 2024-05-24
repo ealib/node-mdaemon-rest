c96c7c66d230bddfd76897212b7498d09a63facb
Emanuele Aliberti <dev@mtka.eu>
2024-05-24 15:08:57 +0200

    increase JWT life span from 5' to 10h

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { jwtConstants } from './constants';
         JwtModule.register({
             global: true,
             secret: jwtConstants.secret,
-            signOptions: { expiresIn: '300s' },
+            signOptions: { expiresIn: '10h' },
         }),
     ],
     providers: [
