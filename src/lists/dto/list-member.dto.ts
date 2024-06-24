030c4a071bba4ac0a11ded99c204f11dabd2874d
Emanuele Aliberti <dev@mtka.eu>
2024-06-24 10:49:56 +0200

    implement a marshaller

--- src/lists/dto/list-member.dto.ts
+++ src/lists/dto/list-member.dto.ts
@@ -1,4 +1,9 @@
-import { MD_ListMember, MdListMemberFlags, MdListMemberMode } from "node-mdaemon-api";
+// node-mdaemon-api
+import {
+    MD_ListMember,
+    MdListMemberFlags,
+    MdListMemberMode,
+} from "node-mdaemon-api";
 
 export class ListMemberDTO implements MD_ListMember {
     Email: string;
@@ -6,4 +11,10 @@ export class ListMemberDTO implements MD_ListMember {
     RealName: string;
     Type: MdListMemberMode;
     Flags?: MdListMemberFlags;
+
+    public static marshal(entity: MD_ListMember): ListMemberDTO {
+        const dto = new ListMemberDTO();
+        Object.assign(dto, entity);
+        return dto;
+    }
 }
